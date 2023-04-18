83fd49c27bce8476e2f0ab79f71141d7d36e0f14
liyu <liyu@weiyankeji.cn>
2023-04-18 16:17:41 +0800

    feat<zuul>:add zuul simpleRouteDemo

---
+++ zuul-proxy/src/main/java/cn/liyu/zuulproxy/Application.java
@@ -0,0 +1,18 @@
+package cn.liyu.zuulproxy;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * <a href="https://developer.aliyun.com/article/754252#slide-5">...</a>
+ *
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
